00ec4a040d2b55297aae0f7b656b8720ce7a6bb7
bnaucler <mail@bnaucler.se>
2017-05-24 21:47:13 +0200

    Text decoration and color options

--- cuttw.c
+++ cuttw.c
@@ -8,11 +8,16 @@
 #define VER "0.1"
 #define TWLEN 140
 #define TAILLEN 9
-#define MXSZ 4096 // Should be enough for any sane twittrer
+#define BBCH 4096 // Should be enough for any sane twittrer
 #define MBCH 1024
 #define SBCH 256
 
+#define WHT				"\033[1m\033[37m"
+#define RED				"\x1B[1m\033[31m"
+#define RESET			"\033[0m"
+
 typedef struct flag {
+	int cfl; // color output
 	int dfl; // text decoration
 	int pfl; // punctuation break flag
 	int vfl; // verbosity
@@ -28,53 +33,55 @@ typedef struct tweet {
 int usage(char *err, const int ret) {
 
 	if(err) fprintf(stderr, "Error: %s\n", err);
-	printf("Usage: cuttw [-dpv] file\n"
+	printf("Usage: cuttw [-cdpv] file\n"
+			"    -c: Colored output\n"
 			"    -d: Text decoration\n"
 			"    -p: Break at punctuation if possible\n"
 			"    -v: Increase verbosity level\n");
 	return ret;
 }
 
-void rbuf(FILE *fp, char *buf, const flag *f) {
+void rbuf(FILE *fp, char *buf) {
 
 	char *sbuf = calloc(MBCH, sizeof(char));
 
-	while(fgets(sbuf, MXSZ, fp)) strncat(buf, sbuf, MBCH);
+	while(fgets(sbuf, BBCH, fp)) strncat(buf, sbuf, MBCH);
 
 	free(sbuf);
 }
 
-void rempref(char *str) {
+void remsp(char *str) {
 
-	while(isspace(*str) || *str == '\n') str++;
+	char *ptr = str;
+	int len = strlen(str);
+	unsigned int ctr = 0;
+
+	while(isspace(*ptr) || *ptr == '\n') {
+		ctr++;
+		ptr++;
+	}
+
+	memmove(str, str + ctr, len - ctr);
+
+	ptr = str + (strlen(str) - 1);
+
+	while(isspace(*ptr) || *ptr == '\n') *ptr-- = 0;
 }
 
-// Fix
-void remtail(char *str, const char rem) {
-
-	char *eptr = str;
-	eptr += (strlen(str) - 1);
-
-	while(*eptr == rem) *eptr-- = 0;
-}
-
-// TODO: get rid of unnecessary if-statement
-int addtail(tweet *head, const int numtw) {
+// TODO: Color
+void addtail(tweet *head, const int numtw, const flag *f) {
 
 	tweet *trav = head;
 	char *tail = calloc(TAILLEN + 1, sizeof(char));
-	int ctr = 1;
+	unsigned int ctr = 0;
 
-	while(*trav->txt) {
+	while(ctr++ < numtw) {
 		snprintf(tail, TAILLEN, " (%d/%d)", ctr, numtw);
 		strncat(trav->txt, tail, TAILLEN);
-		ctr++;
-		if(trav->next) trav = trav->next;
-		else break;
+		trav = trav->next;
 	}
 
 	free(tail);
-	return 0;
 }
 
 int cuttw(tweet *head, char *buf, const flag *f) {
@@ -93,9 +100,7 @@ int cuttw(tweet *head, char *buf, const flag *f) {
 			dptr -= (TWLEN - TAILLEN) - lcut;
 			eptr -= (TWLEN - TAILLEN) - lcut;
 			hasp = lctr = *dptr = 0;
-			rempref(trav->txt);
-			remtail(trav->txt, ' ');
-			remtail(trav->txt, '\n'); // TODO
+			remsp(trav->txt);
 			trav->next = calloc(1, sizeof(tweet));
 			trav = trav->next;
 			dptr = trav->txt;
@@ -107,13 +112,15 @@ int cuttw(tweet *head, char *buf, const flag *f) {
 
 	} while(*++eptr);
 
+	remsp(trav->txt);
+
 	return numtw;
 }
 
 char *spstr(char *str, const int len) {
 
 	char *eptr = str, *lptr = str;
-	int ctr = 0;
+	unsigned int ctr = 0;
 
 	do {
 		if(isspace(*eptr)) lptr = eptr;
@@ -130,12 +137,11 @@ char *spstr(char *str, const int len) {
 }
 
 // TODO:	odd termsize alignment
-//			color
 //			varying alignment if numtw > 9
 char *mkhdr(char *str, char *box, const int boxsz, const int ctr,
 	const int numtw, const flag *f) {
 
-	int sub = 7;
+	int sub = 7; // magic number (space taken by number + decoration)
 	if(numtw > 9) sub += 2;
 
 	int blen = (f->col - sub) / 2;
@@ -145,8 +151,12 @@ char *mkhdr(char *str, char *box, const int boxsz, const int ctr,
 	
 	for(a = 0; a < blen; a++) *ttrav++ = '-';
 
-	snprintf(box, boxsz, "\n%c%s (%d) %s%c\n%s\n",
-			'+', tln, ctr, tln, '+', spstr(str, f->col)); // DEBUG
+	if(f->cfl)
+		snprintf(box, boxsz, RED "\n%c%s " WHT "(%d)" RED " %s%c\n" RESET "%s\n",
+				'+', tln, ctr, tln, '+', spstr(str, f->col));
+	else
+		snprintf(box, boxsz, "\n%c%s (%d) %s%c\n%s\n",
+				'+', tln, ctr, tln, '+', spstr(str, f->col));
 
 	free(tln);
 	return box;
@@ -156,18 +166,18 @@ int prdata(tweet *head, const int numtw, const flag *f) {
 
 	char *box = calloc(MBCH, sizeof(char));
 	tweet *trav = head;
-	int ctr = 1;
+	unsigned int ctr = 0;
 
-	if(f->vfl > 1) printf("DEBUG rows: %d, cols: %d\n\n", f->row, f->col);
+	if(f->vfl > 1 && f->dfl)
+		printf("DEBUG rows: %d, cols: %d\n\n", f->row, f->col);
 
-	while(trav->txt) {
+	while(ctr++ < numtw) {
 		if(f->dfl)
-			printf("%s", mkhdr(trav->txt, box, MBCH, ctr++, numtw, f));
+			printf("%s", mkhdr(trav->txt, box, MBCH, ctr, numtw, f));
 		else
 			printf("%s\n\n", trav->txt);
 
-		if(trav->next) trav = trav->next;
-		else break;
+		trav = trav->next;
 	}
 
 	if(box) free(box);
@@ -176,7 +186,7 @@ int prdata(tweet *head, const int numtw, const flag *f) {
 
 int execop(const flag *f, char **argv) {
 
-	char *buf = calloc(MXSZ, sizeof(char));
+	char *buf = calloc(BBCH, sizeof(char));
 	tweet *head = calloc(1, sizeof(tweet));
 	FILE *fp;
 	int numtw = 0;
@@ -186,9 +196,13 @@ int execop(const flag *f, char **argv) {
 		if(!fp) {
 			return usage("Could not open file", 2);
 		} else {
-			rbuf(fp, buf, f);
+			rbuf(fp, buf);
+			if(!strlen(buf)) {
+				if(f->vfl) fprintf(stderr, "%s: empty file\n", *argv);
+				continue;
+			} else if(f->vfl) printf("%s:\n", *argv);
 			numtw = cuttw(head, buf, f);
-			addtail(head, numtw);
+			addtail(head, numtw, f);
 			prdata(head, numtw, f);
 			fclose(fp);
 		}
@@ -206,6 +220,10 @@ void setfl(char *arg, flag *f) {
 	while(*++arg) {
 		switch(*arg) {
 
+			case 'c':
+				f->cfl++;
+				break;
+
 			case 'd':
 				f->dfl++;
 				break;
@@ -221,21 +239,24 @@ void setfl(char *arg, flag *f) {
 	}
 }
 
+void gettsz(flag *f) {
+
+	struct winsize w;
+	ioctl(0, TIOCGWINSZ, &w);
+	f->row = w.ws_row;
+	f->col = w.ws_col;
+}
+
 int main(int argc, char **argv) {
 
 	flag *f = calloc(1, sizeof(flag));
 
 	if(argc < 2 || (argc == 2 && argv[1][0] == '-'))
 		return usage("No filename provided", 1);
-	else if
-		(argv[1][0] == '-') setfl(argv++[1], f);
+	else if (argv[1][0] == '-')
+		setfl(argv++[1], f);
 
-	if(f->dfl) {
-		struct winsize w;
-		ioctl(0, TIOCGWINSZ, &w);
-		f->row = w.ws_row;
-		f->col = w.ws_col;
-	}
+	if(f->dfl) gettsz(f);
 
 	return execop(f, ++argv);
 
